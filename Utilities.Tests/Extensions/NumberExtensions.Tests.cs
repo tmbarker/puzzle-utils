f66ef04122ac1f9c26371b2e25e6b3d651c3c50b
Trevor Barker <tmbarker9@gmail.com>
2025-11-06 21:37:49 -0800

    Add Ceiling Division to NumberExtensions

--- Utilities.Tests/Extensions/NumberExtensions.Tests.cs
+++ Utilities.Tests/Extensions/NumberExtensions.Tests.cs
@@ -28,4 +28,33 @@ public class NumberExtensionsTests
         // Act & Assert
         Assert.Throws<DivideByZeroException>(() => 10.Modulo(0));
     }
+
+    [Theory]
+    [InlineData(10, 3, 4)]
+    [InlineData(9, 3, 3)]
+    [InlineData(8, 3, 3)]
+    [InlineData(7, 3, 3)]
+    [InlineData(1, 3, 1)]
+    [InlineData(0, 3, 0)]
+    [InlineData(-1, 3, 0)]
+    [InlineData(-2, 3, 0)]
+    [InlineData(-3, 3, 0)]
+    [InlineData(-4, 3, 0)]
+    [InlineData(10, -3, -2)]
+    [InlineData(-10, -3, 4)]
+    public void CeilDiv_ReturnsCorrectResult(int a, int divisor, int expected)
+    {
+        // Act
+        var result = a.CeilDiv(divisor);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void CeilDiv_ZeroDivisor_ThrowsDivideByZeroException()
+    {
+        // Act & Assert
+        Assert.Throws<DivideByZeroException>(() => 10.CeilDiv(0));
+    }
 }
--- Utilities/Extensions/NumberExtensions.cs
+++ Utilities/Extensions/NumberExtensions.cs
@@ -13,7 +13,6 @@ public static class NumberExtensions
     /// <param name="a">The dividend</param>
     /// <param name="modulus">The divisor</param>
     /// <typeparam name="T">The type associated with <paramref name="a" /> and the <paramref name="modulus" /></typeparam>
-    /// <returns>The signed remainder of the division</returns>
     /// <exception cref="DivideByZeroException">The provided <paramref name="modulus" /> is zero</exception>
     public static T Modulo<T>(this T a, T modulus) where T : INumber<T>
     {
@@ -23,5 +22,25 @@ public static class NumberExtensions
         }
         
         return (a % modulus + modulus) % modulus;
+    }
+
+    /// <summary>
+    ///     Returns the ceiling of the division:
+    ///     <para>
+    ///         <b><paramref name="a" />/<paramref name="divisor" /></b>
+    ///     </para>
+    /// </summary>
+    /// <param name="a">The dividend</param>
+    /// <param name="divisor">The divisor</param>
+    /// <typeparam name="T">The type associated with <paramref name="a"/> and <paramref name="divisor"/></typeparam>
+    /// <exception cref="DivideByZeroException">The provided <paramref name="divisor"/> is zero</exception>
+    public static T CeilDiv<T>(this T a, T divisor) where T : IBinaryInteger<T>
+    {
+        if (divisor == T.Zero)
+        {
+            throw new DivideByZeroException();
+        }
+        
+        return (a + divisor - T.One) / divisor;
     }
 }
